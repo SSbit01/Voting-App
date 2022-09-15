41ae5864a07b968ac03fecd38d37e3faca218454
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2022-09-15 17:28:24 +0200

    fix: logout

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -3,5 +3,6 @@ import {withSessionRoute} from "@/lib/withSession"
 export default withSessionRoute(async({session}, res) => {
   session.user = {}
   await session.save()
+  res.setHeader("cache-control", "no-store, max-age=0")
   res.json(session.user)
 })
