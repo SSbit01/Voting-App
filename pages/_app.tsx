30b044754d770bc2a900bf29a30792b759b2244d
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2022-11-09 20:58:07 +0100

    fix: PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,10 +18,10 @@ export default function AppPage({Component, pageProps}: AppProps) {
         <meta charSet="UTF-8"/>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"/>
-        <meta name="description" content="Voting App using Next.js created by SSbit01"/>
+        <meta name="description" content="Voting App made with Next.js created by SSbit01"/>
         <meta name="theme-color" content="#7d1964"/>
 
-        <link rel="icon" type="image/svg+xml" href="/icon.svg" sizes="any"/>
+        <link rel="mask-icon" href="/icon.svg" color="#19327d"/>
         <link rel="shortcut icon" href="/favicon.ico"/>
         <link rel="manifest" href="/manifest.json"/>
 
