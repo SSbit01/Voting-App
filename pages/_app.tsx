7076dd2e82b1b3e476cab5cbb24a643fa22f32e7
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-04-22 17:11:45 +0200

    fix: viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,6 +25,8 @@ export default function AppPage({ Component, pageProps }: AppProps) {
 
         <title>{title}</title>
 
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="minimum-scale=1,initial-scale=1,width=device-width,shrink-to-fit=no,user-scalable=no,viewport-fit=cover" />
         <meta name="application-name" content={title} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
