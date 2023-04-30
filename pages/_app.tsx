38dc4636629bf84c68ffae271eaabcbf1c27b6d9
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2023-05-01 01:22:02 +0200

    refactor: apple-mobile-web-app-status-bar-style to black-translucent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,7 @@ export default function AppPage({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="minimum-scale=1,initial-scale=1,width=device-width,shrink-to-fit=no,user-scalable=no,viewport-fit=cover" />
         <meta name="application-name" content={title} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content={title}/>
         <meta name="description" content={description} />
         <meta name="mobile-web-app-capable" content="yes" />
